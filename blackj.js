13e0ced0c802c817e20bf6fd9834a333ef3fb920
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-04-23 12:59:33 -0700

    first commit/psuedo code

---
+++ blackj.js
@@ -0,0 +1,43 @@
+
+// /*----- constants -----*/
+
+// -create suits, ranks, and deck
+//- probably use arrays
+
+// /*----- app's state (variables) -----*/
+
+// -think about state variables
+// ->turn, player, dealer, played cards, count of cards
+
+// /*----- cached element references -----*/
+
+// -hit, stand, reset buttons
+// -containers for player and dealer cards
+// -win message
+// -dealer and player score
+// -balance, bet amount, etc
+
+// /*----- event listeners -----*/
+
+// -reset button
+// -hit, stand buttons
+// -different for different bets perhaps
+
+// /*----- functions -----*/
+
+// init() -> your initial state, think about:
+// -Initializing new deck
+// -clearing player and dealer cards
+// -clearing messages of some sort
+// -update card count
+// -etc
+
+// Dealer and player turn how would we handle it?
+
+// Drawing cards?
+
+// Adding card to hand?
+
+// Calculating value of your hand?
+
+// Checking win?
