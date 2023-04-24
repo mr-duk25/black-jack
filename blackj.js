4d352d1b206f3e49c847910a7edab863fe828669
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-04-24 11:17:37 -0700

    built deck

--- blackj.js
+++ blackj.js
@@ -1,17 +1,22 @@
 
 // /*----- constants -----*/
 
+
+
 // -create suits, ranks, and deck
 //- probably use arrays
 
 // /*----- app's state (variables) -----*/
-let dealerSum;
-let dealerAceCount;
+// const dealerSum = 0;
+// const playerSum = 0;
 
-let playerSum;
-let playerAceCount;
+// const dealerAceCount = 0; 
+// const playerAceCount = 0;
 
-let turn;
+// const hidden;
+var deck;
+
+const canHit = true; //allows player to hit while sum is < 21;
 
 // -think about state variables
 // ->turn, player, dealer, played cards, count of cards
@@ -31,6 +36,18 @@ let turn;
 // -different for different bets perhaps
 
 // /*----- functions -----*/
+
+function buildDeck() {
+    let values = ["r02", "r03", "r04", "r05", "r06", "r07", "r08", "r09", "r10", "J", "Q", "K", "A"];
+    let suit = ["clubs", "diamonds", "hearts", "spades"];
+    deck = [];
+    for(let i = 0; i < suit.length; i++) {
+        for(let j = 0; j < values.length; j++) {
+            deck.push(suit[i] + "-" + values[j]);
+        }
+    }
+    return deck;
+}
 
 // init() -> your initial state, think about:
 // -Initializing new deck
