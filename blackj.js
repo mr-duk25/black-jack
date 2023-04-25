b0e6a9a423418ad5215c1b9948273498082b50b6
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-04-24 20:27:40 -0700

    card value function added

--- blackj.js
+++ blackj.js
@@ -7,14 +7,17 @@
 //- probably use arrays
 
 // /*----- app's state (variables) -----*/
-// const dealerSum = 0;
-// const playerSum = 0;
+let dealerSum = 0;
+let playerSum = 0;
 
-// const dealerAceCount = 0; 
-// const playerAceCount = 0;
+let dealerAceCount = 0; 
+let playerAceCount = 0;
 
-// const hidden;
-var deck;
+let deck;
+let hidden;
+let dealerCard2;
+let playerCard1;
+let playerCard2;
 
 const canHit = true; //allows player to hit while sum is < 21;
 
@@ -22,7 +25,7 @@ const canHit = true; //allows player to hit while sum is < 21;
 // ->turn, player, dealer, played cards, count of cards
 
 // /*----- cached element references -----*/
-document.querySelectorAll("player-cards")
+
 // -hit, stand, reset buttons
 // -containers for player and dealer cards
 // -win message
@@ -38,10 +41,10 @@ document.querySelectorAll("player-cards")
 // /*----- functions -----*/
 buildDeck();
 shuffleDeck();
-
+// dealCards();
 
 function buildDeck() {
-    let values = ["r02", "r03", "r04", "r05", "r06", "r07", "r08", "r09", "r10", "J", "Q", "K", "A"];
+    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
     let suit = ["clubs", "diamonds", "hearts", "spades"];
     deck = [];
     for(let i = 0; i < suit.length; i++) {
@@ -49,7 +52,7 @@ function buildDeck() {
             deck.push(suit[i] + "-" + values[j]);
         }
     }
-    // return deck;
+   
 }
 
 function shuffleDeck() {
@@ -63,10 +66,48 @@ function shuffleDeck() {
 }
 
 console.log(deck);
+
+// function getValue() {
+//     let card = deck.pop();
+//     let value = card.split("-");
+//     if(isNaN(value[1]) && value[1] === "A") {
+//         return 11;
+//     } else {
+//         return 10;
+//     }
+//     return (parseInt(value[1]));
+// }
+function getValue() {
+    let card = deck.pop();
+    let value = card.split("-");
+    if (isNaN(value[1]) && value[1] === "A") {
+      return 11;
+    } else if (isNaN(value[1])){
+        return 10;
+    } else {
+        return parseInt(value[1])
+    }
+}
     
+   
 
 
-// console.log (deck);
+
+function dealerHand() {
+    hidden = deck.pop();
+    dealerCard2 = deck.pop();
+    dealerHand = [];
+    dealerHand.push(hidden, dealerCard2);
+    return dealerHand;
+}
+function playerHand () {
+    playerCard1 = deck.pop();
+    playerCard2 = deck.pop();
+    playerHand = [];
+    playerHand.push(playerCard1, playerCard2);
+    return playerHand;
+}
+console.log(getValue(playerHand));
 // init() -> your initial state, think about:
 // -Initializing new deck
 // -clearing player and dealer cards
